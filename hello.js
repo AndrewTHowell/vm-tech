a6e08eb064c1d4751203bb096c1981cd4d8b5a3f
Andrew Howell <howelldrew99@gmail.com>
2019-01-28 14:54:33 +0000

    Update hello.js

--- hello.js
+++ hello.js
@@ -10,9 +10,10 @@ app.use(express.static('public'));
 var songs = {"hai": {"fulltitle": "Blah Blah", "type": "atonal"}};
 
 app.get('/songs', function(req, resp){
+
 	const title = req.query.title;
   resp.send('Hello world ' + songs[title].fulltitle);
 })
 
-app.listen(8090);app.listen(8090);
 
+//hello
