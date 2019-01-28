f96818cd7d773fc29c7f870af57fb5dd5ba13cf1
Andrew Howell <howelldrew99@gmail.com>
2019-01-28 22:12:21 +0000

    general improvements

--- app.js
+++ app.js
@@ -9,10 +9,22 @@ app.use(express.static('public'));
 
 var songs = {"hai": {"fulltitle": "Blah Blah", "type": "atonal"}};
 
-app.get('/songs', function(req, resp){
-	var t = req.query.title;
-	console.log(t)
-	resp.send('Hello world' + songs[t].fulltitle);
+var people = {"doctorwhocomposer": {"username":"doctorwhocomposer", "forename":"Delia", "surname":"Derbyshire"}}
+
+app.get('/people', function(req, resp){
+	resp.send(people);
+})
+
+app.get('/people/:username', function(req, resp){
+	var response = people[req.query.username];
+	console.log(people[req.query.username]);
+	resp.send(response);
+})
+
+app.post('/people', function(req, resp){
+	if(!(req.username in people)){
+		people[username] = {"username":req.username, "forename":req.forename, "surname":req.surname}
+	}
 })
 
 module.exports = app;
