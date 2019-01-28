c6a1123cb509b3225e5a3476d272050da973c3a0
Andrew Howell <howelldrew99@gmail.com>
2019-01-28 14:47:10 +0000

    start
    First submission

---
+++ hello.js
@@ -0,0 +1,18 @@
+"use strict"
+
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.use(express.static('public'));
+
+var songs = {"hai": {"fulltitle": "Blah Blah", "type": "atonal"}};
+
+app.get('/songs', function(req, resp){
+	
+	const title = req.query.title;
+  resp.send('Hello world ' + songs[title].fulltitle);
+})
+
+app.listen(8090);
