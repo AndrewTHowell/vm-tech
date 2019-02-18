dacac0c2e5c0812b09d715b8af97364b1c904a90
Andrew Howell <howelldrew99@gmail.com>
2019-02-18 22:41:54 +0000

    Forms now work on any host

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//"use strict"
+"use strict"
 
 const express = require('express');
 const rateLimit = require("express-rate-limit");
@@ -214,5 +214,24 @@ app.post('/answeredMessage', function(req, resp){
 		resp.type('txt').send('Forbidden');
 	}
 })
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+
+  // respond with json
+  if (req.accepts('json')) {
+    res.send({ error: 'Not found' });
+    return;
+  }
+
+  // default to plain-text. send()
+  res.type('txt').send('Not found');
+});
 
 module.exports = app;
